be5239295b3b0205864191664b16c46e1a51a6f3
Nilesh <nilesh070802@gmail.com>
2024-08-18 17:21:31 +0530

    Better string generator

--- Debugger/StringGenerator.cpp
+++ Debugger/StringGenerator.cpp
@@ -21,24 +21,6 @@ string SimpleRandomizedString(int length, char minChar, char maxChar){
     return res;
 }
 
-char getChar(int code){
-    switch(code){
-        case 1:
-            return RandomNum(0, 25) + 'a';
-            break;
-        case 2:
-            return RandomNum(0, 25) + 'A';
-            break;
-        case 3:
-            return RandomNum(0, 9) + '0';
-            break;
-        default:
-            cerr << "Line: " << __LINE__ <<  " = Error : No code found" << endl;
-            break;
-    }
-    return 0;
-}
-
 string SimpleRandomizedString(int length, bool lowercaseAlpha = 0, bool uppercaseAlpha = 0, bool numeric = 0, vector<char> extraChar = {}){
     /*
         Input Parameters:
@@ -54,22 +36,29 @@ string SimpleRandomizedString(int length, bool lowercaseAlpha = 0, bool uppercas
     */
     string res;
 
-    vector<int> need;
-    if(lowercaseAlpha) need.push_back(1);
-    if(uppercaseAlpha) need.push_back(2);
-    if(numeric) need.push_back(3);
-    if(!extraChar.empty()) need.push_back(4);
+    vector<char> Chars;
+    if(lowercaseAlpha) {
+        for(char i = 'a'; i <= 'z'; i++){
+            Chars.push_back(i);
+        }
+    }
+    if(uppercaseAlpha) {
+        for(char i = 'A'; i <= 'Z'; i++){
+            Chars.push_back(i);
+        }
+    }
+    if(numeric) {
+        for(char i = '0'; i <= '9'; i++){
+            Chars.push_back(i);
+        }
+    }
+    for(auto &c : extraChar){
+        Chars.push_back(c);
+    }
 
     for(int i = 0; i < length; i++){
-        int index = RandomNum(0, (int)need.size() - 1);
-        char c;
-        if(need[index] == 4){
-            c = extraChar[RandomNum(0, (int)extraChar.size() - 1)];
-        }
-        else{
-            c = getChar(need[index]);
-        }
-        res += c;
+        int index = RandomNum(0, (int)Chars.size() - 1);
+        res += Chars[index];
     }
     return res;
 }
